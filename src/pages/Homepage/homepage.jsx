7cb52e27518437b85e83ac0f5e1b0fcd2ac43281
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-02-21 16:47:52 -0300

    aula 4

--- src/pages/Homepage/homepage.jsx
+++ src/pages/Homepage/homepage.jsx
@@ -5,51 +5,51 @@ import {
   Slogan,
   Eventos,
   EventosLista,
-  EventosItem,
-  EventoTitulo,
-  EventoImagem,
-  EventoDataHora,
 } from "./homepage.styles";
+
 import axios from "axios";
 
-import Cabecalho from '../../components/Cabecalho/cabecalho'
-import Rodape from '../../components/Rodape/rodape'
+import Cabecalho from "../../components/Cabecalho/cabecalho";
+import Rodape from "../../components/Rodape/rodape";
+import Card from "../../components/Card/card";
+
 function Homepage() {
+  const [eventos, setEventos] = useState([]);
 
-    const [eventos, setEventos] = useState([]);
-
-    useEffect(() => {
-        const listarEventos = async () => {
-            try {
-                const response = await axios.get('http://localhost:3000/eventos');
-                setEventos(response.data);
-            } catch (error) {
-                console.error('Erro ao buscar eventos', error);
-            }
-        }
-        listarEventos();
-    })
-    return (
-        <>
-            <Cabecalho />
-            <Container>
-                <Title>Bem vindo a Casa de eventos!</Title>
-                <Slogan>Essa é a casa de festas que realiza sonhos.</Slogan>
-                <Eventos>
-                    <EventosLista>
-                        { eventos.map((evento, index) => (
-                        <EventosItem key={index}>
-                            <EventoTitulo>{ evento.titulo }</EventoTitulo>
-                            <EventoImagem src={ evento.url_da_imagem } alt={ evento.titulo } />
-                            <br />
-                            <EventoDataHora>Data e Hora: { evento.data } - { evento.horario }</EventoDataHora>
-                        </EventosItem>
-                        ))}
-                    </EventosLista>
-                </Eventos>
-            </Container>
-            <Rodape />
-        </>
-    )
+  useEffect(() => {
+    const listarEventos = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/eventos");
+        setEventos(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar eventos", error);
+      }
+    };
+    listarEventos();
+  });
+  return (
+    <>
+      <Cabecalho />
+      <Container>
+        <Title>Bem vindo a Casa de eventos!</Title>
+        <Slogan>Essa é a casa de festas que realiza sonhos.</Slogan>
+        <Eventos>
+          <EventosLista>
+            {eventos.map((evento, index) => (
+              <Card
+                key={index}
+                titulo={evento.titulo}
+                urlImagem={evento.url_da_imagem}
+                data={evento.data}
+                horario={evento.horario}
+                id={evento.id}
+              />
+            ))}
+          </EventosLista>
+        </Eventos>
+      </Container>
+      <Rodape />
+    </>
+  );
 }
-export default Homepage
+export default Homepage;
--- src/pages/Homepage/homepage.styles.jsx
+++ src/pages/Homepage/homepage.styles.jsx
@@ -1,57 +1,31 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 1140px;
-  height: calc(100vh - 240px);
-  display: block;
-  margin: auto;
-`;
+    width: 1140px;
+    height: calc(100vh - 240px);
+    display: block;
+    margin: auto;
+`
 export const Title = styled.h1`
-  font-family: "Playfair Display", serif;
-  padding-top: 58px;
-  font-size: 36px;
-  font-weight: 400;
-  color: #1f0049;
-`;
+    font-family: 'Playfair Display', serif;
+    padding-top: 58px;
+    font-size: 36px;
+    font-weight: 400;
+    color: #1F0049;
+`
 export const Slogan = styled.p`
-  font-family: "Playfair Display", serif;
-  font-size: 24px;
-  line-height: 2em;
-  color: #6f9f9c;
-`;
+    font-family: 'Playfair Display', serif;
+    font-size: 24px;
+    line-height: 2em;
+    color: #6F9F9C;
+`
 
-export const Eventos = styled.div``;
+export const Eventos = styled.div`
+   
+`
 export const EventosLista = styled.ul`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  padding: 20px 0 40px 0;
-`;
-export const EventosItem = styled.li`
-  width: 360px;
-  min-height: 320px;
-  background-color: #ffffff;
-  -webkit-box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
-  -moz-box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
-  box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
-  list-style-type: none;
-`;
-export const EventoTitulo = styled.h3`
-  font-family: "Playfair Display", serif;
-  font-size: 18px;
-  padding: 15px 15px 0 15px;
-  font-weight: 300;
-  line-height: 150%;
-`;
-
-export const EventoImagem = styled.img`
-  padding: 15px;
-  width: 100%;
-  aspect-ratio: 16/10;
-  object-fit: cover;
-`;
-export const EventoDataHora = styled.p`
-  font-size: 14px;
-  padding: 15px;
-  line-height: 150%;
-`;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    padding: 20px 0 40px 0;
+`
